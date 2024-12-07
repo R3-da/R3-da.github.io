4d9143212178c781495fa88233a992f22335d51d
r3-da <haddan.reda.net@gmail.com>
2024-12-07 03:36:02 +0100

    - fix fluid tailwind after getting overriden by shadcn - install prettier-plugin-tailwindcss - add favicon title and description metadata and temp images assets - improve fluid tailwind on hero section - add temp layout to be improved later

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,18 @@ import { Nav } from '@/components/nav';
 import './globals.css'; // Import global CSS
 import Footer from '@/components/footer';
 import { Providers } from '@/lib/provider';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Reda Haddan | Portfolio',
+	description: 'Reda Haddan Portfolio Website',
+	icons: {
+		icon: '/favicon-96x96.png',
+		shortcut: '/favicon.ico',
+		apple: '/apple-touch-icon.png',
+	},
+	manifest: '/site.webmanifest',
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
